de40d25f65e191b1e25756c39ebe867d6d19ffe5
Void <pedroaa57@gmail.com>
2019-11-13 12:49:35 -0300

    Add Matrix, Vector and Triangle class

--- HTML/script.js
+++ HTML/script.js
@@ -1,3 +1,267 @@
+// function to calculate a determinant of a 2x2
+function det2(a, b, c, d) {
+    return(a*d - b*c);
+}
+
+// generate 2d list with default value
+function create_matrix(y, x, value = 0) {
+    var matrix = [];
+    var row = [];
+    for (let y_index = 0; y_index < y; y_index++) {
+        row = [];
+        for (let x_index = 0; x_index < x; x_index++) {
+            row.push(value);
+        }
+        // push to the matrix
+        matrix.push(row);
+    }
+    // return matrix
+    return matrix;
+}
+
+// vector class
+class Vector {
+    constructor(data = null, length = 0) {
+        if (data == null) {
+            data = [];
+            for (let index = 0; index < length; index++) {
+                data.push(0);
+            }
+        }
+        this.data = data;
+    }
+
+    cross(other) {
+        if (other instanceof Vector) {
+            // create matrix
+            var matrix = new Matrix(3, 3, [[new Vector([1, 0, 0]), new Vector([0, 1, 0]), new Vector([0, 0, 1])], this.data.slice(), other.data.slice()], "V");
+            // return the determinant
+            return matrix.det();
+        } else {
+            throw "Not a Vector";
+        }
+    }
+
+
+    mul(other) {
+        if (other instanceof Vector) {
+            if (other.data.length == this.data.length) {
+                var sum = 0;
+                for (let index = 0; index < this.data.length; index++) {
+                    sum += this.data[index]*other.data[index];
+                }
+            }
+            return sum;
+        } else {
+            for (let index = 0; index < this.data.length; index++) {
+                this.data[index] *= other;
+            }
+        } 
+        return this;
+    }
+
+    add(other) {
+        if (other instanceof Vector) {
+            for (let index = 0; index < Math.min(other.data.length, this.data.length); index++) {
+                this.data[index] += other.data[index];  
+            }
+        } else {
+            for (let index = 0; index < this.data.length; index++) {
+                this.data[index] += other;
+            }
+        }
+        return this;
+    }
+
+    sub(other) {
+        if (other instanceof Vector) {
+            for (let index = 0; index < Math.min(other.data.length, this.data.length); index++) {
+                this.data[index] -= other.data[index];  
+            }
+        } else {
+            for (let index = 0; index < this.data.length; index++) {
+                this.data[index] -= other;
+            }
+        }
+        return this;
+    }
+
+    div(scalar) {
+        for (let index = 0; index < this.data.length; index++) {
+            this.data[index] /= scalar;
+        }
+        return this;
+    }
+
+}
+
+
+// matrix class
+class Matrix {
+    constructor(rows, cols, data = null, dtype = "N") {
+        this.nrows = rows;
+        this.ncols = cols;
+        // if data is null, generate from cols && rows
+        if (data == null) {
+            data = [];
+            for (let row = 0; row < rows; row++) {
+                var data_row = [];
+                for (let col = 0; col < cols; col++) {
+                    // add 0 or 1 to make a Indentity matrix
+                    if (col == row) {
+                        data_row.push(1);
+                    } else {
+                        data_row.push(0);
+                    }
+                }
+                // add data row to data itthis
+                data.push(data_row);
+            }
+        }
+        // set data
+        this.data = data;
+        this.dtype = dtype;
+    }
+
+    // method to calculate the determinant
+    det() {
+        // check if is a square matrix
+        if (this.nrows != this.ncols) {
+            throw "Not a Square matrix";
+        } else {
+            // check type
+            if (this.nrows == 2) {
+                return det2(this.data[0][0], this.data[0][1], this.data[1][0], this.data[1][1]);
+            } else if (this.nrows == 3) {
+                var det_0 = det2(this.data[1][1], this.data[1][2], this.data[2][1], this.data[2][2]);
+                var det_1 = det2(this.data[1][0], this.data[1][2], this.data[2][0], this.data[2][2]);
+                var det_2 = det2(this.data[1][0], this.data[1][1], this.data[2][0], this.data[2][1]);
+                // check type
+                if (this.dtype == "N") {
+                    return this.data[0][0]*det_0 - this.data[0][1]*det_1 + this.data[0][2]*det_2
+                } else {
+                    // copy
+                    var i_vector = new Vector(this.data[0][0].data.slice());
+                    var j_vector = new Vector(this.data[0][1].data.slice());
+                    var k_vector = new Vector(this.data[0][2].data.slice());
+                    return i_vector.mul(det_0).sub(  j_vector.mul(det_1) ).add(  k_vector.mul(det_2) );
+                }
+                
+            } else {
+                throw "Cant Calculate Inverse for that many dimensions";
+            }
+        }
+    }
+
+    // function to calculate the inveser o a 2x2 matrix
+    inv() {
+        if ( (this.nrows != this.ncols) || (this.dtype != "N") ) {
+            throw "Not a Valid Matrix";
+        } else if (this.nrows == 2 ) {
+            // get determinant
+            var det = this.det();
+            // check det
+            if (det == 0) {
+                throw "Not Inversible";
+            } else {
+                // return matrix
+                return new Matrix(2, 2, [[ this.data[1][1]/det, -this.data[0][1]/det], [ -this.data[1][0]/det, this.data[0][0]/det ]]);
+            }
+        } else if (this.nrows == 3 ) {
+            // get cofactors
+            var A = det2(this.data[1][1], this.data[1][2], this.data[2][1], this.data[2][2]);
+            var B = -det2(this.data[1][0], this.data[1][2], this.data[2][0], this.data[2][2]);
+            var C = det2(this.data[1][0], this.data[1][1], this.data[2][0], this.data[2][1]);
+
+            var D = -det2(this.data[0][1], this.data[0][2], this.data[2][1], this.data[2][2]);
+            var E = det2(this.data[0][0], this.data[0][2], this.data[2][0], this.data[2][2]);
+            var F = -det2(this.data[0][0], this.data[0][1], this.data[2][0], this.data[2][1]);
+
+            var G = det2(this.data[0][1], this.data[0][2], this.data[1][1], this.data[1][2]);
+            var H = -det2(this.data[0][0], this.data[0][2], this.data[1][0], this.data[1][2]);
+            var I = det2(this.data[0][0], this.data[0][1], this.data[1][0], this.data[1][1]);
+
+            // get determinant
+            var det = this.det();
+            // use formula: A⁽⁻¹⁾ = det(A)⁽⁻¹⁾ * [Cofactor Matrix]^T
+            var transpose = new Matrix(3, 3, [[A, D, G], [B, E, H], [C, F, I]] );
+            return transpose.div(det);
+        } else {
+            throw "Cant Calculate Inverse for that many dimensions";
+        }
+    }
+
+    print() {
+        // for each row
+        for (let index = 0; index < this.data.length; index++) {
+            console.log(this.data[index]);   
+        }
+    }
+
+    mul(other) {
+        if (other.nrows != this.ncols) {
+            throw "Invalid Multiplication"
+        } else {
+            // do the multiplication
+            // create matrix 
+            var matrix = create_matrix(this.nrows, other.ncols );
+            // apply the dot algorithm
+            for (let i = 0; i < this.nrows; i++) {
+                for (let j = 0; j < other.ncols; j++) {
+                    for (let k = 0; k < this.ncols; k++) {
+                        matrix[i][j] += this.data[i][k]*other.data[k][j];
+                    }
+                }
+            }
+            return new Matrix(this.nrows, other.ncols, matrix);
+        }
+    }
+
+    div(scalar) {
+        for (let row = 0; row < this.nrows; row++) {
+            for (let col = 0; col < this.ncols; col++) {
+                this.data[row][col] = this.data[row][col]/scalar;
+            }
+        }
+        return this;
+    }
+}
+
+
+
+class Triangle {
+    constructor(p1, p2, p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    to_barycentric(p) {
+        // cnvert to matrix as homogean coordinates
+        var cartesian = new Matrix(3, 1, [[p.x,], [p.y,], [1,]]);
+        var triangle = new Matrix(3, 3, [[this.p1.x, this.p2.x, this.p3.x], [this.p1.y, this.p2.y, this.p3.y], [1, 1, 1]]);
+        // formula : lambda = T⁽⁻¹⁾ * P
+        var inv = triangle.inv();
+        return inv.mul(cartesian);
+    }
+
+
+    // function to check if it in in a triangle
+    in_triangle(point, log=true) {
+        // convert to barycentric
+        var coordinates = this.to_barycentric(point);
+        // check if there is at least one negative
+        for (let coord = 0; coord < coordinates.data.length; coord++) {
+            if (coordinates.data[coord] < 0) {
+                return true;
+            }
+        }
+        // if none was negative, retarne true
+        return true;
+
+    }
+}
+
 // class to represent a Rotation
 class Rotation {
 
@@ -80,6 +344,11 @@ function create_matrix(y, x, value = 0) {
     }
     // return matrix
     return matrix;
+}
+
+// function to calculate a determinant of a 2x2
+function det2(a, b, c, d) {
+    return(a*c - b*d);
 }
 
 // matrix multiplication
